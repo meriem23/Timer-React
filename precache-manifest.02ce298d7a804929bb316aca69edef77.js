502ccffc25c71f1c142d4566b7860edf40a9ad51
meriem23 <esra.msallem@gmail.com>
2020-03-03 11:38:41 +0100

    Updates

---
+++ precache-manifest.02ce298d7a804929bb316aca69edef77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb1fef6c421a47092f145efe473a92c",
+    "url": "/Timer-React/index.html"
+  },
+  {
+    "revision": "b0c2b9825aca9d046318",
+    "url": "/Timer-React/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "50dbe6ef9bae0595a113",
+    "url": "/Timer-React/static/css/main.0e736d1a.chunk.css"
+  },
+  {
+    "revision": "b0c2b9825aca9d046318",
+    "url": "/Timer-React/static/js/2.0a120991.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Timer-React/static/js/2.0a120991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50dbe6ef9bae0595a113",
+    "url": "/Timer-React/static/js/main.ef496143.chunk.js"
+  },
+  {
+    "revision": "ba97342c199b40cff0ce",
+    "url": "/Timer-React/static/js/runtime-main.2f3a0281.js"
+  }
+]);
